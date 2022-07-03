37afe79fb3c29d8279a7ae3efc4facdc462e52d6
Abner Palhano <abnerpalhano@alunos.utfpr.edu.br>
2022-07-03 18:37:11 -0300

    Added an About page and stylized the Department crud pages

--- Data/SalesWebContext.cs
+++ Data/SalesWebContext.cs
@@ -5,12 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 
-    public class SalesWebContext : DbContext
+public class SalesWebContext : DbContext
+{
+    public SalesWebContext(DbContextOptions<SalesWebContext> options)
+        : base(options)
     {
-        public SalesWebContext (DbContextOptions<SalesWebContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<SalesWebMvc.Models.Departments>? Departments { get; set; }
     }
+
+    public DbSet<SalesWebMvc.Models.Departments> Departments { get; set; }
+}
--- Models/Departments.cs
+++ Models/Departments.cs
@@ -1,13 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
     public class Departments
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
